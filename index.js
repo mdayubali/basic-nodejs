70641f2fbc4b72f8d12626607855a0910f84e5c1
mdayubali <ayubh9288@gmail.com>
2022-07-09 16:33:54 +0600

    update the code

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//Middleware
+app.use(cors());
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    res.send("Hello world from node js");
+    res.send("Hello world from node js and enjoy the expess framework");
 })
 
 app.listen(port, () => {
